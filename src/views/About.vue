71874cb947186616340a7b55bd38c37613e8286e
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-08-19 20:55:30 +0900

    About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-flex xs12 md5 offset-md2>
+    <h1 class="text-xs-center">This is an about page</h1>
+  </v-flex>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
