6b22d747882963c0080875a604d01252ed607100
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-08-19 20:55:14 +0900

    Nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <v-app light>
     <v-content>
-      <router-view/>
+      <v-container>
+        <v-layout row wrap align-center>
+          <Nav></Nav>
+          <router-view/>
+        </v-layout>
+      </v-container>
     </v-content>
     <v-footer>
       <span>&copy; 2018</span>
     </v-footer>
   </v-app>
 </template>
+
+<script>
+import Nav from '@/components/Nav'
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-flex xs12 md4>
+    <div class="text-xs-center">
+      <v-avatar size="125px">
+        <img
+          class="img-circle elevation-7 mb-1"
+          src="https://lovemewithoutall.github.io/assets/images/binary.jpg"
+        >
+      </v-avatar>
+      <div class="headline">what <span style="font-weight:bold">Books</span></div>
+      <div class="subheading text-xs-center grey--text pt-1 pb-3">All we read</div>
+      <v-layout justify-space-between>
+        <router-link to="/" class="body-2">Home</router-link>
+        <router-link to="/about" class="body-2">About</router-link>
+        <a href="javascript:;" class="body-2">Write</a>
+        <a href="javascript:;" class="body-2">Login</a>
+        <a href="javascript:;" class="body-2">Logout</a>
+      </v-layout>
+    </div>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
