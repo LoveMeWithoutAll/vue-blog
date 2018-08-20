fb7e6366626aec8636630824d804aaa3bc43e223
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-08-20 22:50:01 +0900

    Register Auth watcher

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,18 +1,11 @@
 import firebase from 'firebase'
-import { SET_USER } from './mutation_types'
-
-const setUser = ({commit}, user) => {
-  commit(SET_USER, user.user)
-}
 
 const signIn = (store, {email, password}) => {
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
     .then(
-      (user) => {
-        setUser(store, user)
-      },
+      (user) => {},
       (err) => {
         alert('Oops. ' + err.message)
       }
@@ -23,9 +16,7 @@ const signOut = (store) => {
   firebase
     .auth()
     .signOut()
-    .then(() => {
-      setUser(store, null)
-    })
+    .then()
     .catch((error) => console.error(`SingOut Error: ${error}`))
 }
 
