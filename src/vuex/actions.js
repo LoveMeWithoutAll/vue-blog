f04f5500d7764953cbd51ad3e5c69c382d128cca
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-09-06 18:29:48 +0900

    Refactoring login

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,30 +1,11 @@
 import firebase from 'firebase'
 
-const signIn = (store, {email, password}) => {
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(
-      (user) => {},
-      (err) => {
-        alert('Oops. ' + err.message)
-      }
-    )
-}
-
-const signOut = (store) => {
-  firebase
-    .auth()
-    .signOut()
-    .then()
-    .catch((error) => console.error(`SingOut Error: ${error}`))
-}
-
 export default {
-  login (store, {email, password}) {
-    signIn(store, {email, password})
-  },
-  logout (store) {
-    signOut(store)
+  logout () {
+    firebase
+      .auth()
+      .signOut()
+      .then()
+      .catch((error) => console.error(`SingOut Error: ${error}`))
   }
 }
