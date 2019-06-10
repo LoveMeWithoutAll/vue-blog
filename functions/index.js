d49b74fddd4f7eb1c53885d2594b82c2d38cb08c
YoungSeon.Ahn <ys.ahn@outlook.com>
2019-06-10 11:11:34 +0900

    Security update

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions')
 const mkdirp = require('mkdirp-promise')
-const gcs = require('@google-cloud/storage')()
+const gcs = require('@google-cloud/storage')
 const spawn = require('child-process-promise').spawn
 const path = require('path')
 const os = require('os')
